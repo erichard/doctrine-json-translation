86d43717e18396f1def531f6e709d75208c81c06
Erwan Richard <erwan.richard@lephare.com>
2017-12-28 12:33:50 +0100

    Better form type

--- src/Bridge/Symfony/Form/TranslatedType.php
+++ src/Bridge/Symfony/Form/TranslatedType.php
@@ -1,28 +1,57 @@
 <?php
 
-namespace Erichard\DoctrineJsonTranslation\Bridge\Symfony\Form;
+namespace AdminBundle\Form;
 
 use Erichard\DoctrineJsonTranslation\TranslatedField;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class TranslatedType extends AbstractType
 {
+    protected $availableLocales;
+
+    public function __construct($availableLocales)
+    {
+        $this->availableLocales = $availableLocales;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('fr', TextType::class, ['label' => false]);
+        foreach ($this->availableLocales as $locale) {
+            $builder->add($locale, $options['entry_type'], ['label' => false]);
+        }
+
         $builder->addViewTransformer(new CallbackTransformer(
-            function (TranslatedField $fieldAsObject = null) {
-                return null === $fieldAsObject ? [] : $fieldAsObject->all();
+            function ($fieldAsObject = null) {
+                if (null === $fieldAsObject) {
+                    return null;
+                } elseif (is_array($fieldAsObject)) {
+                    return $fieldAsObject;
+                } elseif ($fieldAsObject instanceof TranslatedField) {
+                    return $fieldAsObject->all();
+                } else {
+                    throw new \InvalidArgumentException();
+                }
             },
-            function (array $fieldAsArray) {
+            function ($fieldAsArray) {
                 return new TranslatedField($fieldAsArray);
             }
         ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'entry_type' => TextType::class,
+        ]);
     }
 }
